8a10495887be6531b35baf5785cc7a301c908cd2
jaimin patel <jaiminp2k6@gmail.com>
2026-08-02 11:37:20 +0530

    Time: 2 ms (96.92%), Space: 43.9 MB (62.92%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i=num1.length() -1;
+        int j=num2.length() -1;
+        int carry = 0;
+
+        StringBuilder sb = new StringBuilder();
+        
+        while(i >= 0 || j >= 0 ||carry != 0){
+            int dig1 = 0;
+            int dig2 = 0;
+            if(i >= 0){
+                dig1 = num1.charAt(i) - '0';
+                i--;
+            }if(j >= 0){
+                dig2 = num2.charAt(j) - '0';
+                j--;
+            }
+            int sum = dig1+dig2+carry;
+
+            int dig = sum % 10;
+            carry = sum / 10;
+            sb.append(dig);
+        }
+
+        return sb.reverse().toString();
+    }
+}
